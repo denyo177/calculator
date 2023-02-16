b05f59323486c0222debbea7473de452ddc2c47e
Dennis <121116755+denyo177@users.noreply.github.com>
2023-02-16 16:16:25 +0100

    Function woking so far

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ const divide = function (a, b) {
 
 const operate = function (operator, a, b) {
 
+     a = parseInt(a);
+     b = parseInt(b);
+
     switch (operator) {
         case '+':
             return add(a, b);
@@ -27,28 +30,91 @@ const operate = function (operator, a, b) {
             return divide(a, b);
     }
 }
-let displayVal = "";
 
-const displayNum = function (btn) {
-    let id = btn.getAttribute('id');
-    btn.addEventListener('click', function() {
-        const divDisplay = document.querySelector('.display');
-        divDisplay.textContent += id;
-        displayVal = divDisplay.textContent;
-        console.log(displayVal);
-    });
-    
+const clearDisplay = function() {
+    const divDisplay = document.querySelector('.display');
+    divDisplay.textContent = "";
+};
+const addDisplayValue = function (value) {
+    const divDisplay = document.querySelector('.display');
+    divDisplay.textContent += value;
+};
+const getDisplayValue = function () {
+    const divDisplay = document.querySelector('.display');
+    return divDisplay.textContent;
 };
 
-// const keys = Array.from(document.querySelectorAll(".key"));
-// keys.forEach(key => key.addEventListener('click'), displayNum() );
+let a = "0";
+let b = "0";
+let op = "";
+let calcStage = 0; // 0: initial 1: get 1st value 2: get op 3: get 2nd value, calculate and display
+addDisplayValue("0");
 
-document.querySelectorAll('.key.number,#clear').forEach(button => displayNum(button));
+const main = function (e) {
+    
+    if (e.target.id === "clear") {
+        clearDisplay();
+        a = b = "0";
+        addD
+    isplayValue("0");
+        calcStage = 0;
+    };
+    if (calcStage === 0) {
+        if (e.target.className === "key number") {
+            clearDisplay();
+            addD
+        isplayValue(e.target.id);
+            calcStage = 1;
+        };
+    }  else if (calcStage === 1) {
+        if (e.target.className === "key number") {
+            addD
+        isplayValue(e.target.id);
+        };
 
+        if (e.target.className === "key operator") {
+            op = e.target.id;
+            a = getDisplayValue();
+            calcStage = 2;
+        };
+    } else if (calcStage === 2) {
+        if (e.target.className === "key operator") {
+            op = e.target.id;
+        };
 
-// window.addEventListener('click', displayNum);
+        if (e.target.className === "key number") {
+            clearDisplay(); // wahrscheinlich nicht wenn aus der loop kommend
+            addD
+        isplayValue(e.target.id);
+            calcStage = 3;
+        };
+    } else if (calcStage === 3) {
+        if (e.target.className === "key number") { 
+            addD
+        isplayValue(e.target.id);
+        };
 
+        if (e.target.className === "key operator") {
+            a = operate(op, a, getDisplayValue());
+            clearDisplay();
+            addD
+        isplayValue(a);
+            op = e.target.id;
+            calcStage = 2;
+        };
 
-// key pressed : this numer in display
-//eventlistener auf alle keys. den gewählten dann auslesen und 
-// in display div mit der nr erstellen
+        if (e.target.id === "enter") {
+            b = getDisplayValue();
+            const result = operate(op, a, b);
+            clearDisplay();
+            addD
+        isplayValue(result);
+            a = result;
+            b = "0";
+            calcStage = 1;
+        }; 
+    };
+};
+
+window.addEventListener('click', main);
+
