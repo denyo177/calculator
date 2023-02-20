5c1d24f2e2ad4fc5a5f4a9fe4dcd0f3561c8d23b
Dennis <121116755+denyo177@users.noreply.github.com>
2023-02-20 07:38:43 +0100

    Working. Added error when divide by 0, ongoing calc with float numbers.

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ const divide = function (a, b) {
 
 const operate = function (operator, a, b) {
 
-     a = parseInt(a);
-     b = parseInt(b);
+     a = parseFloat(a);
+     b = parseFloat(b);
 
     switch (operator) {
         case '+':
@@ -44,6 +44,17 @@ const getDisplayValue = function () {
     return divDisplay.textContent;
 };
 
+function limitAndRoundNumber(number) {
+    if (Math.round(number).toString().length > 9) {
+        return "overflow";
+    } else {
+        const wholeDigits = Math.round(number).toString().length;
+        const power = wholeDigits <= 8 ? 8 - wholeDigits : 0;
+        const multiplier = 10**power;
+        return Math.round(number * multiplier) / multiplier;
+    };
+};
+
 let a = "0";
 let b = "0";
 let op = "";
@@ -56,22 +67,21 @@ const main = function (e) {
         clearDisplay();
         a = b = "0";
         addDisplayValue("0");
+        const divDisplay = document.querySelector('.display');
+        divDisplay.classList.remove('nullWarning');
         calcStage = 0;
     };
     if (calcStage === 0) {
         if (e.target.className === "key number") {
             clearDisplay();
             addDisplayValue(e.target.id);
+            const divDisplay = document.querySelector('.display');
+            divDisplay.classList.remove('nullWarning');
             calcStage = 1;
         };
     }  else if (calcStage === 1) {
-        if (e.target.className === "key number") {
+        if (e.target.className === "key number" && getDisplayValue().length < 9) {
             addDisplayValue(e.target.id);
-        };
-        if (getDisplayValue().length > 9) {
-            const tooLongValue = getDisplayValue();
-            clearDisplay();
-            addDisplayValue(tooLongValue.substring(1));
         };
 
         if (e.target.className === "key operator") {
@@ -85,36 +95,55 @@ const main = function (e) {
         };
 
         if (e.target.className === "key number") {
-            clearDisplay(); // wahrscheinlich nicht wenn aus der loop kommend
-            addDisplayValue(e.target.id);
-            calcStage = 3;
+            clearDisplay(); 
+            if (e.target.id === "0") {
+                addDisplayValue("What, Really? ...  Are You Ready for infinity ??"); 
+                const divDisplay = document.querySelector('.display');
+                divDisplay.classList.add('nullWarning');
+                calcStage = 0;
+            } else {
+                addDisplayValue(e.target.id);
+                calcStage = 3;
+            };
+            
         };
     } else if (calcStage === 3) {
-        if (e.target.className === "key number") { 
+        if (e.target.className === "key number" && getDisplayValue().length < 9) { 
             addDisplayValue(e.target.id);
         };
 
         if (e.target.className === "key operator") {
-            a = operate(op, a, getDisplayValue());
+            a = limitAndRoundNumber(operate(op, a, getDisplayValue()));
             clearDisplay();
             addDisplayValue(a);
-            op = e.target.id;
-            calcStage = 2;
+            if (a === "overflow") {
+                calcStage = 0;
+            } else {
+                op = e.target.id;
+                calcStage = 2;
+            };
         };
 
         if (e.target.id === "enter") {
             b = getDisplayValue();
-            const result = operate(op, a, b);
+            const result = limitAndRoundNumber(operate(op, a, b));
             clearDisplay();
             addDisplayValue(result);
-            a = result;
-            b = "0";
-            calcStage = 1;
-        }; 
+            if (result === "overflow") {
+                calcStage = 0;
+                a = b = "0";
+            } else {
+                a = result;
+                b = "0";
+                calcStage = 1;
+            }; 
+        };
     };
 };
 
 window.addEventListener('click', main);
 
-//maximum 9 ziffern darstellen
+//maximum 9 ziffern darstellen auch an anderen stellen im code
+// add button highlighting
+
 
